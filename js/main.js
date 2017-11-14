d8aee516004a0117eb64749cef6fdc36971e36ca
blackcezar <75moneylove75@gmail.com>
2017-11-14 22:51:18 +0700

    Init 2

--- js/main.js
+++ js/main.js
@@ -5,13 +5,11 @@ $(document).ready(function () {
     paper.install(window);
     paper.setup(document.getElementById('mainCanvas'));
 
-    var c;
-    for (var x=25; x<400; x+=50) {
-        for (var y=25; y<400; y+=50) {
-            c = Shape.Circle(x,y,20);
-            c.fillColor='green';
-        }
-    }
+    var tool = new Tool();
+    tool.onMouseDown=function (event) {
+        var c = Shape.Circle(event.point.x,event.point.y, 20);
+        c.fillColor='green';
+    };
 
 
     paper.view.draw();
