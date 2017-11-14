79bbcbf6d000201b3312694e5460a4194863b071
blackcezar <75moneylove75@gmail.com>
2017-11-14 22:39:22 +0700

    Init 2

---
+++ js/main.js
@@ -0,0 +1,22 @@
+$(document).ready(function () {
+    'use strict';
+    console.log('main.js loaded');
+
+    paper.install(window);
+    paper.setup(document.getElementById('mainCanvas'));
+
+    var c;
+    for (var x=25; x<400; x+=50) {
+        for (var y=25; y<400; y+=50) {
+            c = Shape.Circle(x,y,20);
+            c.fillColor='green';
+        }
+    }
+
+
+    paper.view.draw();
+
+
+
+
+});
