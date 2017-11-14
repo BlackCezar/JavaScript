a5a4f909f72e4432ef7b9ae4a9be07d9343c165b
blackcezar <75moneylove75@gmail.com>
2017-11-14 22:57:33 +0700

    Init 2

--- js/main.js
+++ js/main.js
@@ -5,6 +5,13 @@ $(document).ready(function () {
     paper.install(window);
     paper.setup(document.getElementById('mainCanvas'));
 
+    var c = Shape.Circle(200,200,80);
+    c.fillColor='black';
+    var text = new PointText(200,200);
+    text.justification='center';
+    text.fillColor='white';
+    text.fontSize=20;
+    text.content='Hello world';
     var tool = new Tool();
     tool.onMouseDown=function (event) {
         var c = Shape.Circle(event.point.x,event.point.y, 20);
